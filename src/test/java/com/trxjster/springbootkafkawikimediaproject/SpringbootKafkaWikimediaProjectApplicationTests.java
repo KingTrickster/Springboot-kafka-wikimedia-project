e8140e42c816c6207f192c2fd6c9ae5136c1efd8
Alejandro Trejo <alextrejores@gmail.com>
2023-06-08 23:22:50 -0600

    First commit

---
+++ src/test/java/com/trxjster/springbootkafkawikimediaproject/SpringbootKafkaWikimediaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trxjster.springbootkafkawikimediaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWikimediaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
